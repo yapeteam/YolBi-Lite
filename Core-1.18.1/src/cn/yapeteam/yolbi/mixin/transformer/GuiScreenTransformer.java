f5242dfcb23596372368f0586eb4d326e47a8b0a
TIMER_err <zqq2333@outlook.com>
2024-07-27 11:11:31 +0800

    fixed

--- Core-1.18.1/src/cn/yapeteam/yolbi/mixin/transformer/GuiScreenTransformer.java
+++ Core-1.18.1/src/cn/yapeteam/yolbi/mixin/transformer/GuiScreenTransformer.java
@@ -3,6 +3,7 @@ package cn.yapeteam.yolbi.mixin.transformer;
 import cn.yapeteam.ymixin.ASMTransformer;
 
 import cn.yapeteam.yolbi.YolBi;
+import cn.yapeteam.yolbi.event.impl.player.EventChat;
 import net.minecraft.client.gui.screens.Screen;
 
 import org.objectweb.asm_9_2.Opcodes;
@@ -13,15 +14,16 @@ public class GuiScreenTransformer extends ASMTransformer {
     public GuiScreenTransformer() {
         super(Screen.class);
     }
-    @Inject(method = "sendMessage",desc = "(Ljava/lang/String;Z)V")
-    public void onMessage(MethodNode methodNode){
-        InsnList list=new InsnList();
-        LabelNode label=new LabelNode();
-        list.add(new VarInsnNode(Opcodes.ALOAD,1));
+
+    @Inject(method = "sendMessage", desc = "(Ljava/lang/String;Z)V")
+    public void onMessage(MethodNode methodNode) {
+        InsnList list = new InsnList();
+        LabelNode label = new LabelNode();
+        list.add(new VarInsnNode(Opcodes.ALOAD, 1));
         //list.add(new MethodInsnNode(Opcodes.INVOKESPECIAL,Type.getInternalName(String.class),"toString","()Ljava/lang/String;"));
         //list.add(new LdcInsnNode(false));//
-        list.add(new MethodInsnNode(Opcodes.INVOKESTATIC, Type.getInternalName(GuiScreenTransformer.class),"handler","(Ljava/lang/String;)Z"));
-        list.add(new JumpInsnNode(Opcodes.IFEQ,label));
+        list.add(new MethodInsnNode(Opcodes.INVOKESTATIC, Type.getInternalName(GuiScreenTransformer.class), "handler", "(Ljava/lang/String;)Z"));
+        list.add(new JumpInsnNode(Opcodes.IFEQ, label));
         //Opcodes.INVOKE
         //methodNode.visitMaxs(0,0);
         list.add(new InsnNode(Opcodes.RETURN));
@@ -29,8 +31,10 @@ public class GuiScreenTransformer extends ASMTransformer {
 
         methodNode.instructions.insert(list);
     }
-    public static boolean handler(String msg){
-        return ;
-        //todo bind handler
+
+    public static boolean handler(String msg) {
+        EventChat eventChat = new EventChat(msg);
+        YolBi.instance.getEventManager().post(eventChat);
+        return eventChat.isCancelled();
     }
 }
