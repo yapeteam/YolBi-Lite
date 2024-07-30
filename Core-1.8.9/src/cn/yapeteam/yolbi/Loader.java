0cb6d6899782117ccc29ab6b6566cc0b1706900e
TIMER_err <zqq2333@outlook.com>
2024-07-30 22:19:06 +0800

    VersionInfo

--- Core-1.8.9/src/cn/yapeteam/yolbi/Loader.java
+++ Core-1.8.9/src/cn/yapeteam/yolbi/Loader.java
@@ -13,7 +13,7 @@ public class Loader {
         try {
             if (JVMTIWrapper.instance == null)
                 JVMTIWrapper.instance = new NativeWrapper();
-            if (BootStrap.getVersion().first != VersionInfo.V1_8_9) {
+            if (BootStrap.getVersion().first != Version.V1_8_9) {
                 Logger.error("Unsupported Minecraft version: {}", BootStrap.getVersion().first.getVersion());
                 SocketSender.send("CLOSE");
                 return;
