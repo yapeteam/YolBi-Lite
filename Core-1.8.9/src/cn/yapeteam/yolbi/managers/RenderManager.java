768ea4cd96702e266acf3ce0ea3d6406af0ef36e
neilhuang007 <neilhuang007@gmail.com>
2024-07-30 21:43:54 +0800

    Yolbi UI base drawn

--- Core-1.8.9/src/cn/yapeteam/yolbi/managers/RenderManager.java
+++ Core-1.8.9/src/cn/yapeteam/yolbi/managers/RenderManager.java
@@ -76,24 +76,6 @@ public class RenderManager {
     public static byte[] renderToBitmap() {
         // Clear the canvas before drawing
         canvas.clear(new java.awt.Color(0, 0, 0, 0).getRGB());
-//        // Draw a rounded rectangle with clipping
-//        drawRoundedRect(0, 0, 800, 600, 15, 0xFF171A21);
-//
-//        drawRect(0, 0, 200, 600, 0xFF121419);
-//
-//        drawRoundedRect(225, 50, 550, 100, 15, 0xFF121419);
-//
-//        Font Nunito20 = FontManager.instance.getNunito20();
-//        drawText("type here to search", Nunito20, 225, 30, 0xFFA0A0A0);
-//
-//        drawText("Yolbi", FontManager.instance.getSans32(), 28, 36 + 16, 0xFFFFFFFF);
-//
-//        drawText("1.0beta", FontManager.instance.getSans16(), 102, 32, 0xFFA0A0A0);
-//
-//        drawImage(img, 28, 82, 24, 24);
-//
-//        drawText("Combat", Nunito20, 60, 100.5, 0xFFFFFFFF);
-
         Data data = EncoderPNG.encode(surface.makeImageSnapshot());
         return data != null ? data.getBytes() : null;
     }
