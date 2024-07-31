379178b024a84fb479067cd2352c05e953eda9b0
neilhuang007 <neilhuang007@gmail.com>
2024-07-31 21:15:59 +0800

    Verification Gui push

---
+++ Injector/src/cn/yapeteam/injector/VerificationGUI.java
@@ -0,0 +1,24 @@
+package cn.yapeteam.injector;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class VerificationGUI {
+    private JButton loginButton;
+    private JPanel panel1;
+    private JTextField textField1;
+    private JCheckBox saveCredentialsCheckBox;
+    private JPasswordField passwordField1;
+    private JProgressBar progressBar1;
+    private JTextPane loggingInTextPane;
+
+    public VerificationGUI() {
+        loginButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+}
---
+++ Injector/src/cn/yapeteam/injector/verification/RSAHandler.java
@@ -0,0 +1,4 @@
+package cn.yapeteam.injector.verification;
+
+public class RSAHandler {
+}
