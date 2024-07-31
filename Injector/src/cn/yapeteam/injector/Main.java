3c74413c1e7c92550d6c5391c596927efe30e85e
TIMER_err <zqq2333@outlook.com>
2024-07-31 22:51:56 +0800

    frame

--- Injector/src/cn/yapeteam/injector/Main.java
+++ Injector/src/cn/yapeteam/injector/Main.java
@@ -12,15 +12,31 @@ public class Main {
     public static final String dllName = "libinjection.dll";
     public static final String agentName = "agent.jar";
     public static final int port = 20181;
-    public static int pid;
 
     public static void main(String[] args) throws Exception {
+        SplashScreen splashScreen = new SplashScreen();
+        splashScreen.display();
         Utils.unzip(Main.class.getResourceAsStream("/injection.zip"), YolBi_Dir);
         if (OS.isFamilyWindows())
             System.load(new File(Main.YolBi_Dir, "libapi.dll").getAbsolutePath());
         UIManager.setLookAndFeel(new FlatXcodeDarkIJTheme());
-        VerificationGUI frame = new VerificationGUI();
-        new Thread(() -> frame.setVisible(true)).start();
-        pid = Integer.parseInt(args[1]);
+        LoginFrame frame = new LoginFrame((a, b) -> {
+            MainFrame mainFrame = new MainFrame();
+            new Thread(() -> mainFrame.setVisible(true)).start();
+            if (args.length == 2) {
+                switch (args[0]) {
+                    case "dll":
+                        mainFrame.inject_dll(Integer.parseInt(args[1]));
+                        mainFrame.inject_ui();
+                        break;
+                    case "agent":
+                        mainFrame.inject_agent(args[1]);
+                        mainFrame.inject_ui();
+                }
+            }
+            return true;
+        });
+        splashScreen.close();
+        frame.setVisible(true);
     }
 }
