7468e6814c25389a58ad3e153de441cecd5d5bad
TIMER_err <zqq2333@outlook.com>
2024-08-03 18:11:59 +0800

    修复webui

--- Loader/dll/src/auth/build.bat
+++ Loader/dll/src/auth/build.bat
@@ -1,3 +1,3 @@
 @echo off
-g++ -shared -s verifyNative.cpp -Iinclude -Llib libcrypto-3-x64.dll libcurl.dll libjsoncpp.dll C:\Windows\System32\IPHLPAPI.DLL -o libauth.dll
+g++ -s ddos.cpp -Iinclude -Lthread -Llib libcrypto-3-x64.dll libcurl.dll libjsoncpp.dll C:\Windows\System32\IPHLPAPI.DLL -o ddos.exe -finput-charset=UTF-8 -fexec-charset=GBK
 pause
