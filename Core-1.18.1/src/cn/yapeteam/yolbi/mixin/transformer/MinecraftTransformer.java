9acb06bddc3e00783f7909987a37d60bcd466ddf
FengeDamc <fengedamc@outlook.com>
2024-08-03 12:56:40 +0800

    fix cubk&yaomao's litter antiheike

--- Core-1.18.1/src/cn/yapeteam/yolbi/mixin/transformer/MinecraftTransformer.java
+++ Core-1.18.1/src/cn/yapeteam/yolbi/mixin/transformer/MinecraftTransformer.java
@@ -1,11 +1,15 @@
 package cn.yapeteam.yolbi.mixin.transformer;
 
 import cn.yapeteam.ymixin.ASMTransformer;
+import cn.yapeteam.ymixin.utils.DescParser;
+import cn.yapeteam.ymixin.utils.Mapper;
 import cn.yapeteam.yolbi.YolBi;
 import cn.yapeteam.yolbi.event.impl.game.EventTick;
 import net.minecraft.client.Minecraft;
 import org.objectweb.asm_9_2.Opcodes;
 import org.objectweb.asm_9_2.Type;
+import org.objectweb.asm_9_2.tree.FieldInsnNode;
+import org.objectweb.asm_9_2.tree.InsnNode;
 import org.objectweb.asm_9_2.tree.MethodInsnNode;
 import org.objectweb.asm_9_2.tree.MethodNode;
 
@@ -17,6 +21,12 @@ public class MinecraftTransformer extends ASMTransformer {
     @Inject(method = "runTick",desc = "(Z)V")//Minecraft/runTick (Z)V
     public void runTick(MethodNode methodNode){
         methodNode.instructions.insert(new MethodInsnNode(Opcodes.INVOKESTATIC, Type.getInternalName(MinecraftTransformer.class),"onRunTick","()V"));
+    }
+    @Inject(method = "getInstance",desc = "()Lnet/minecraft/client/Minecraft;")
+    public void getInstance(MethodNode methodNode){
+        methodNode.instructions.clear();
+        methodNode.instructions.add(new FieldInsnNode(Opcodes.GETFIELD, Mapper.getObfClass("net/minecraft/client/Minecraft"), Mapper.map("instance","net/minecraft/client/Minecraft","Lnet/minecraft/client/Minecraft;", Mapper.Type.Field),DescParser.mapDesc("Lnet/minecraft/client/Minecraft;")));
+        methodNode.instructions.add(new InsnNode(Opcodes.ARETURN));
     }
     public static void onRunTick(){
         YolBi.instance.getEventManager().post(new EventTick());
