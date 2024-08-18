b29d25912e4d2e3e3aebb7be1bc72637c2963e46
neilhuang007 <neilhuang007@gmail.com>
2024-08-18 20:06:28 +0800

    fixed sidebar

--- Core-1.8.9/src/cn/yapeteam/yolbi/utils/render/GuiUtil.java
+++ Core-1.8.9/src/cn/yapeteam/yolbi/utils/render/GuiUtil.java
@@ -1,6 +1,7 @@
 package cn.yapeteam.yolbi.utils.render;
 
 import cn.yapeteam.yolbi.utils.vector.Vector2d;
+import cn.yapeteam.yolbi.utils.vector.Vector2f;
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
@@ -11,5 +12,13 @@ public class GuiUtil {
 
     public boolean mouseOver(final Vector2d position, final Vector2d scale, final double mouseX, final double mouseY) {
         return mouseX > position.x && mouseX < position.x + scale.x && mouseY > position.y && mouseY < position.y + scale.y;
+    }
+
+    public boolean mouseOver(final Vector2f position, final Vector2f scale, final double mouseX, final double mouseY) {
+        return mouseX > position.x && mouseX < position.x + scale.x && mouseY > position.y && mouseY < position.y + scale.y;
+    }
+
+    public boolean mouseOver(final Vector2f position, final Vector2f scale, final Vector2d mouse) {
+        return mouse.x > position.x && mouse.x < position.x + scale.x && mouse.y > position.y && mouse.y < position.y + scale.y;
     }
 }
